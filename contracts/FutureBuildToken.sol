60945a3aa534a506fbc41a0dd178b3ea21f13943
Ionut <ionut.gingu@gmail.com>
2023-01-08 16:45:09 +0200

    Linting.

--- contracts/FutureBuildToken.sol
+++ contracts/FutureBuildToken.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FutureBuildToken is ERC20 {
-    string constant TOKEN_NAME = "FutureBuildToken";
-    string constant TOKEN_SYMBOL = "FTRTKN";
-    uint8 constant DECIMALS = 21;
+  string constant TOKEN_NAME = "FutureBuildToken";
+  string constant TOKEN_SYMBOL = "FTRTKN";
+  uint8 constant DECIMALS = 21;
 
-    uint256 constant INITIAL_SUPPLY_IN_TOKENS = 10**8;
-    uint256 constant INITIAL_SUPPLY_IN_UNITS =
-        INITIAL_SUPPLY_IN_TOKENS * (10**DECIMALS);
+  uint256 constant INITIAL_SUPPLY_IN_TOKENS = 10**8;
+  uint256 constant INITIAL_SUPPLY_IN_UNITS =
+    INITIAL_SUPPLY_IN_TOKENS * (10**DECIMALS);
 
-    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
-        _mint(msg.sender, INITIAL_SUPPLY_IN_UNITS);
-    }
+  constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
+    _mint(msg.sender, INITIAL_SUPPLY_IN_UNITS);
+  }
 
-    function decimals() public view virtual override returns (uint8) {
-        return DECIMALS;
-    }
+  function decimals() public view virtual override returns (uint8) {
+    return DECIMALS;
+  }
 }
--- contracts/Staking/IStaking.sol
+++ contracts/Staking/IStaking.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.9;
 
 interface IStaking {
-    function stake(uint256 amount) external;
+  function stake(uint256 amount) external;
 
-    function unstake(uint256 amount) external;
+  function unstake(uint256 amount) external;
 
-    function claim(uint256 amount) external;
+  function claim(uint256 amount) external;
 }
